bd7ce05a7809bb9f140fc9b57fcc9b99b0a329bf
Dembickaya-ya <alexandra_44@mail.ru>
2024-06-03 12:32:05 +0300

    Домашнее задание №4
    Написание кода на добавление новых элементов в список, зачёркивание элементов и так далее в соответствии с заданием

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const sendInput = document.querySelector('#input');
+const newElement = document.querySelector('#items');
+
+sendInput.addEventListener('keydown', function (event) {
+    const elementText = sendInput.value;
+
+    const newParagraph = document.createElement('div');
+    newParagraph.textContent = elementText;
+
+    if (event.key == 'Enter') {
+        elementText != '';
+        newElement.append(newParagraph);
+        sendInput.value = '';
+        newParagraph.addEventListener('click', function () {
+            newParagraph.classList.toggle('done');
+        });
+    } 
+})
+
+
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
 /* Пустые элементы не должны добавляться */
